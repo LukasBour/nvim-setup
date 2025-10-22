ceee21e4de2cc41948d86094f699de1a1f67c751
Lukas Bauer <lukas.bauer1@stud.tu-darmstadt.de>
2025-10-22 13:40:53 +0200

    Fixed workspace symbol search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,4 +6,8 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
-vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols, {})
+vim.keymap.set("n", "<leader>fS", function()
+	local query = vim.fn.input("Workspace symbols > ")
+	if query == nil or query == "" then return end
+	builtin.lsp_workspace_symbols({ query = query })
+end, {})
